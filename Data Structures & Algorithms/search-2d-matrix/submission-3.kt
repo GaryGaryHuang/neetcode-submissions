0265bde2a625556e143459a269925acfa1d37466
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-05-30 06:23:32 +0200

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.kt
@@ -0,0 +1,42 @@
+class Solution {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        var left = 0
+        var right = matrix.size - 1
+        var targetRow = -1
+        while (left <= right) {
+            val mid = (left + right) / 2
+            when {
+                matrix[mid][0] == target -> return true
+                matrix[mid][0] > target -> {
+                    right = mid - 1
+                    targetRow = mid - 1
+                }
+                else -> {
+                    left = mid + 1
+                    targetRow = mid
+                }
+            }
+        }
+
+        if (targetRow < 0 || targetRow >= matrix.size) return false
+
+        left = 0
+        right = matrix[targetRow].size - 1
+        while (left <= right) {
+            val mid = (left + right) / 2
+            when {
+                matrix[targetRow][mid] == target -> return true
+                matrix[targetRow][mid] > target -> right = mid - 1
+                else -> left = mid + 1
+            }
+        }
+
+        return false
+    }
+}
+
+/**
+Each row is sorted in non-decreasing order, 
+so use binary search to find the row that likely contains the target. 
+Then search for the target within that row.
+*/
