8b7ccd33551a38fbdbba658e9c6724bbbdfaa0dd
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-06-14 11:02:28 +0200

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val subsets = mutableListOf<List<Int>>()
+
+        fun backtracking(subset: MutableList<Int>, start: Int) {
+            subsets.add(subset.toList())
+            if (start == nums.size) return
+            for (i in start until nums.size) {
+                subset.add(nums[i])
+                backtracking(subset, i + 1)
+                subset.remove(nums[i])
+            }
+        }
+
+        backtracking(mutableListOf(), 0)
+        return subsets
+    }
+}
