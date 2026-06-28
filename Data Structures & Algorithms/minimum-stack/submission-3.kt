dc29c871972fe71fee9cd869a6534e2b8959f1bb
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-06-28 15:06:23 +0200

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.kt
@@ -0,0 +1,21 @@
+class MinStack() {
+    //  value, min
+    private val deque = ArrayDeque<Pair<Int, Int>>()
+
+    fun push(`val`: Int) {
+        val min = if (deque.isEmpty()) `val` else minOf(`val`, deque.last().second)
+        deque.addLast(Pair(`val`, min))
+    }
+
+    fun pop() {
+        deque.removeLast()
+    }
+
+    fun top(): Int {
+        return deque.last().first
+    }
+
+    fun getMin(): Int {
+        return deque.last().second
+    }
+}
