d65f88f7dac98dcfd167e233ab86f1450f40341b
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-05 11:36:43 +0200

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.kt
@@ -0,0 +1,23 @@
+class TimeMap() {
+    private val keyToValues = mutableMapOf<String, MutableList<Pair<Int, String>>>()
+
+    fun set(key: String, value: String, timestamp: Int) {
+        keyToValues.getOrPut(key) { mutableListOf() }.add(Pair(timestamp, value))
+    }
+
+    fun get(key: String, timestamp: Int): String {
+        val values = keyToValues[key] ?: return ""
+        var left = 0
+        var right = values.size - 1
+        var ans = -1
+        while (left <= right) {
+            val mid = (left + right) / 2
+            if (values[mid].first > timestamp) right = mid - 1
+            else {
+                ans = left
+                left = mid + 1
+            }
+        }
+        return if (ans == -1) "" else values[ans].second
+    }
+}
