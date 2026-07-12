2114645d9feb55b5571a0cc2a86f87701405b78e
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-12 11:38:40 +0800

    Add: combination-target-sum - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum/submission-7.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun combinationSum(nums: IntArray, target: Int): List<List<Int>> {
+        nums.sort()
+
+        val set = mutableListOf<List<Int>>()
+        val current = mutableListOf<Int>()
+        fun dfs(start: Int, sum: Int) {
+            if (sum == target) {
+                set.add(current.toList())
+                return
+            }
+
+            for (i in start until nums.size) {
+                if (sum + nums[i] > target) {
+                    break
+                }
+
+                current.add(nums[i])
+                dfs(i, sum + nums[i])
+                current.removeLast()
+            }
+        }
+
+        dfs(0, 0)
+        return set
+    }
+}
