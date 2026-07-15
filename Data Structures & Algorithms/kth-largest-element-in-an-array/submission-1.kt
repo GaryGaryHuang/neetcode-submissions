ede77ea9584a410ca71a7bb81b61394ba46c6fa4
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-15 16:46:07 +0800

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun findKthLargest(nums: IntArray, k: Int): Int {
+        val pq = PriorityQueue<Int>()
+        for (num in nums) {
+            pq.offer(num)
+            if (pq.size > k) pq.poll()
+        }
+        return pq.peek()
+    }
+}
