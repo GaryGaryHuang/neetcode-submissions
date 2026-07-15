f75931fbdccfe72f4b563beec67eeed2963c9e13
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-15 16:31:25 +0800

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun lastStoneWeight(stones: IntArray): Int {
+        val pq = PriorityQueue<Int>(reverseOrder())
+        for (s in stones) {
+            pq.offer(s)
+        }
+
+        while (pq.size > 1) {
+            val x = pq.poll()
+            val y = pq.poll()
+            if (x != y) {
+                pq.offer(x - y)
+            }
+        }
+        return pq.peek()
+    }
+}
