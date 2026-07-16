6aa3ef3614e1304f103a574fd36fa49841a34ef0
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-16 09:29:01 +0800

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.kt
@@ -0,0 +1,36 @@
+class Twitter {
+    private val userToTweets = mutableMapOf<Int, ArrayDeque<Tweet>>()
+    private val userToFollowees = mutableMapOf<Int, MutableSet<Int>>()
+    private var timstamp = 0L
+
+    fun postTweet(userId: Int, tweetId: Int) {
+        userToTweets.getOrPut(userId) { ArrayDeque()}.apply {
+            add(Tweet(tweetId, timstamp++))
+            if (size > 10) removeFirst()
+        }
+    }
+
+    fun getNewsFeed(userId: Int): List<Int> {
+        val pq = PriorityQueue<Tweet>(compareByDescending { it.timestamp })
+        userToTweets.get(userId)?.let { pq.addAll(it) }
+        userToFollowees.get(userId)?.forEach { followeeId ->
+            userToTweets.get(followeeId)?.let { pq.addAll(it) }
+        }
+        
+        val res = mutableListOf<Int>()
+        while (pq.isNotEmpty() && res.size < 10) {
+            res.add(pq.poll().id)
+        }
+        return res
+    }
+
+    fun follow(followerId: Int, followeeId: Int) {
+        userToFollowees.getOrPut(followerId) { mutableSetOf() }.add(followeeId)
+    }
+
+    fun unfollow(followerId: Int, followeeId: Int) {
+        userToFollowees.get(followerId)?.remove(followeeId)
+    }
+}
+
+data class Tweet(val id: Int, val timestamp: Long)
