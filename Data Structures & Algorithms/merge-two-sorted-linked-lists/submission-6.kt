ad84f41ec59ff2769bfe0da3bbf12817ad169f3b
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-31 14:04:05 +0800

    Add: merge-two-sorted-linked-lists - submission-6

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-6.kt
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        val dummy = ListNode(0)
+
+        var node1 = list1
+        var node2 = list2
+        var current = dummy
+        while (node1 != null && node2 != null) {
+            if (node1.`val` <= node2.`val`) {
+                current.next = node1
+                node1 = node1.next
+            } else {
+                current.next = node2
+                node2 = node2.next
+            }
+            current = current.next!!
+        }
+
+        current.next = node1 ?: node2
+
+        return dummy.next
+    }
+}
