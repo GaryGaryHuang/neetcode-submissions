e7f07b0ffe8da8ee0da569104666394a98b54b57
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-07-31 13:28:14 +0800

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.kt
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        var res: Int? = null
+        var count = 0
+
+        fun dfs(node: TreeNode?): Boolean {
+            if (node == null) return false
+            if (dfs(node.left)) return true
+
+            count++
+            if (count == k) {
+                res = node.`val`
+                return true
+            }
+            return dfs(node.right)
+        }
+
+        dfs(root)
+        return res!!
+    }
+}
