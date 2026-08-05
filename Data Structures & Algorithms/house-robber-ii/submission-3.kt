3189cf8318bb105c7c0bac97505b552ae8859847
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-08-05 17:01:11 +0800

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0]
+
+        val dp1 = IntArray(nums.size)
+        dp1[0] = nums[0]
+        dp1[1] = maxOf(nums[0], nums[1])
+        for (i in 2 until nums.size - 1) {
+            dp1[i] = maxOf(dp1[i - 2] + nums[i], dp1[i - 1])
+        }
+
+        val dp2 = IntArray(nums.size)
+        dp2[0] = 0
+        dp2[1] = nums[1]
+        for (i in 2 until nums.size) {
+            dp2[i] = maxOf(dp2[i - 2] + nums[i], dp2[i - 1])
+        }
+
+        return maxOf(dp1[nums.size - 2], dp2[nums.size - 1])
+    }
+}
+
+/**
+1. Could rob the first house, but cannot rob the last
+2. Cannot rob the first house, but could rob the last
+*/
