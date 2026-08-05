7503915e1565336728f411025a858386d9fee9e6
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-08-05 16:39:48 +0800

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+        if (nums.size == 1) return nums[0]
+        if (nums.size == 2) return maxOf(nums[0], nums[1])
+        val dp = IntArray(nums.size)
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        for (i in 2 until nums.size) {
+            dp[i] = maxOf(dp[i - 2] + nums[i], dp[i - 1])
+        }
+        return dp.last()
+    }
+}
+
+/**
+cannot rob two adjacent houses
+*/
