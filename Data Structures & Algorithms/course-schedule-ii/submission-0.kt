df36464df4574fcac78f53ac3a2532e077d7a982
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-08-11 09:52:46 +0800

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.kt
@@ -0,0 +1,34 @@
+class Solution {
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+        val courseToPreCourses = mutableMapOf<Int, MutableList<Int>>()
+        for ((course, preCourse) in prerequisites) {
+            courseToPreCourses.getOrPut(course) { mutableListOf() }.add(preCourse)
+        }
+
+        val visited = mutableSetOf<Int>()
+        fun dfs(course: Int): List<Int> {
+            if (course in visited) return emptyList()
+            if (course !in courseToPreCourses) return listOf(course)
+
+            visited.add(course)
+            val paths = mutableListOf<Int>()
+            for (preCourse in courseToPreCourses[course]!!) {
+                val path = dfs(preCourse)
+                if (path.isEmpty()) return emptyList()
+                paths.addAll(path)
+            }
+            visited.remove(course)
+            paths.add(course)
+            courseToPreCourses.remove(course)
+            return paths
+        }
+
+        val res = mutableListOf<Int>()
+        for (i in 0 until numCourses) {
+            val path = dfs(i)
+            if (path.isEmpty()) return intArrayOf()
+            else res.addAll(path)
+        }
+        return res.toIntArray()
+    }
+}
