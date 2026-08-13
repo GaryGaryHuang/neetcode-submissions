1bbb235fba2a2066d074b01f5b9375d8a2f49ca1
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-08-13 15:22:00 +0800

    Add: intersection-of-two-linked-lists - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-0.kt
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+        var nodeA = headA
+        var nodeB = headB
+        while (nodeA != nodeB) {
+            if (nodeA == null) nodeA = headB
+            else nodeA = nodeA.next
+
+            if (nodeB == null) nodeB = headA
+            else nodeB = nodeB.next
+        }
+        return nodeA
+    }
+}
