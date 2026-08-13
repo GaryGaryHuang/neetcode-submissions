58935b1eb46656a5b09a3b0ff7219246abb692fb
Gary Huang <97072317+GaryGaryHuang@users.noreply.github.com>
2026-08-13 16:19:21 +0800

    Add: palindrome-linked-list - submission-1

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-1.kt
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun isPalindrome(head: ListNode?): Boolean {
+        if (head == null || head.next == null) return true
+
+        var slow = head
+        var fast = head
+        while (fast?.next != null) {
+            slow = slow!!.next
+            fast = fast.next?.next
+        }
+
+        var prev: ListNode? = null
+        var node: ListNode? = slow
+        while (node != null) {
+            val temp = node.next
+            node.next = prev
+            prev = node
+            node = temp
+        }
+
+        var nodeA = head
+        var nodeB = prev
+        while (nodeB != null) {
+            if (nodeA!!.`val` != nodeB.`val`) return false
+            nodeA = nodeA.next
+            nodeB = nodeB.next
+        }
+        return true
+    }
+}
